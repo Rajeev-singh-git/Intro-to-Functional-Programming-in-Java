9b9bde22e604c17c4c1ec1df36d99a35d66170d8
Rajeev Singh <rajeevsinghup30@gmail.com>
2022-07-28 16:33:14 +0530

    String Exercise

---
+++ src/programming/FPO1Exercise.java
@@ -0,0 +1,37 @@
+package programming;
+
+import java.util.List;
+
+public class FPO1Exercise {
+	
+public static void main(String[] args) {
+		List<String> courses = List.of("Spring", "Spring Boot", "API", "Microservices", "AWS", "Azure", "Docker", "Git", "PCF");
+		System.out.println("All Courses ");
+		printAllCoursesInFunctional(courses);
+		System.out.println("Courses only containing spring word");
+		printSpringCoursesInFunctional(courses);
+		System.out.println("Courses containing at least 4 letters");
+		printCoursesHavingAtLeastFourLetterInFunctional(courses);
+	}
+
+
+	private static void printAllCoursesInFunctional(List<String> courses) {
+	courses.stream()
+	.forEach(System.out::println);
+	}
+
+	private static void printSpringCoursesInFunctional(List<String> courses) {
+	 courses.stream()
+	 .filter(course->course.contains("Spring"))
+	 .forEach(System.out::println);
+	} 
+ 
+	private static void printCoursesHavingAtLeastFourLetterInFunctional(List<String> courses) {
+	  courses.stream()
+	 .filter(course-> course.length() >= 4)
+	 .forEach(System.out::println);
+		
+	}
+
+
+}
--- src/programming/FPO1Functional.java
+++ src/programming/FPO1Functional.java
@@ -1,18 +1,22 @@
 package programming;
 
 import java.util.List;
-import java.util.function.Predicate;
 
 public class FPO1Functional {
 
 	public static void main(String[] args) {
 		
 		List<Integer> numbers = List.of(12,9,18,16,12,36,39);
+		System.out.println("All Numbers ");
 		printAllNumbersInFunctional(numbers);
+		System.out.println("Even Numbers only");
 		printAllEvenNumbersInFunctional(numbers);
+		System.out.println("Odd Numbers only");
+		printAllOddNumbersInFunctional(numbers);
 	}
 
-	private static void printAllNumbersInFunctional(List<Integer> numbers) {
+
+   private static void printAllNumbersInFunctional(List<Integer> numbers) {
 		//Our focus is  =  What to do?
 		numbers.stream()
 		.forEach(System.out::println); //Method Reference
@@ -21,11 +25,15 @@ public class FPO1Functional {
 	private static void printAllEvenNumbersInFunctional(List<Integer> numbers) {
 		//Our focus is  =  What to do?
 		numbers.stream()
-		  .filter(number -> number%2 ==0)  //Lambda expression
+		  .filter(number -> number%2 ==0)  //Lambda Expression
 		  .forEach(System.out::println);   //Method Reference
 	}
-
-
 	
 
+	private static void printAllOddNumbersInFunctional(List<Integer> numbers) {
+	   numbers.stream()
+	   .filter(number -> number%2 !=0)
+	   .forEach(System.out::println);  
+	}
+	
 }
